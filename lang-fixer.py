69840b477aa905c085c19d17b0f4fff55eafc010
Yarin <15931306+TheYarin@users.noreply.github.com>
2022-03-24 20:53:04 +0200

    replaced almost all the usage of pynput with the keyboard package because it's much more convenient

--- lang-fixer.py
+++ lang-fixer.py
@@ -1,80 +1,47 @@
 import sys
 from time import sleep
-from typing import Union
-from pynput.keyboard import Key, Controller, Listener, GlobalHotKeys
+import keyboard as kb
+from pynput.keyboard import Controller, Key
 import pyperclip
 
-from replacer import replacer, layout1, detectLayout
+from replacer import replacer
 
 keyboardController = Controller()
 
-class LayoutAwareTyper:
-    def __init__(self, currentLayout):
-        self._isEnglish = currentLayout == layout1
-        self._initialIsEnglish = self._isEnglish
 
-    def type(self, text):
-        for c in text:
-            if c in layout1:
-                self._switchToEnglish()
-            else:
-                self._switchToHebrew()
+def copySelectedText():
+    copyKey = 'cmd' if sys.platform == 'darwin' else 'ctrl'
+    kb.send(f'{copyKey}+c')
 
-            keyboardController.tap(c)
-    
-    def _changeLanguage(self):
-        # TODO add mac/linux support
-        with keyboardController.pressed(Key.alt_l):
-            keyboardController.tap(Key.shift_l)
-        self._isEnglish = not self._isEnglish
-    
-    def _switchToEnglish(self):
-        if not self._isEnglish:
-            self._changeLanguage()
-    
-    def _switchToHebrew(self):
-        if self._isEnglish:
-            self._changeLanguage()
 
-    def switchToOtherLayout(self):
-        if (self._initialIsEnglish):
-            self._switchToHebrew()
-        else:
-            self._switchToEnglish()
+def changeLanguage():
+    kb.send('alt+shift')  # TODO add support for macOS
 
-def on_activate():
-    '''Defines what happens on press of the hotkey'''
-    print("Activated!")
+
+def selectTextFromCursorToStartOfLine():
+    # kb.send('shift+home') # doesn't work, a weird shift key up event of a shift key with scan code of 554 is sent immediately after the shift and it's weirdddd
+    with keyboardController.pressed(Key.shift):  # Using pynput here because of the bug with the `keyboard` package
+        keyboardController.tap(Key.home)
+        keyboardController.tap(Key.home)  # again, to catch entire word-wrapped line
+
+
+def on_hotkey():
     prevClipboardContent = pyperclip.paste()
 
-    # Select from cursor to home
-    with keyboardController.pressed(Key.shift):
-        keyboardController.tap(Key.home)
-        keyboardController.tap(Key.home) # again, to catch entire word-wrapped line
+    selectTextFromCursorToStartOfLine()
 
-    # Copy
-    copyKey = Key.cmd if sys.platform == 'darwin' else Key.ctrl
-    with keyboardController.pressed(copyKey):
-        keyboardController.tap('c')
-
-    sleep(0.1) # otherwise the copying may not be finished before pasting
+    copySelectedText()
+    sleep(0.1)  # otherwise the copying may not be finished before pasting
 
     copiedText = pyperclip.paste()
 
     newText = replacer(copiedText)
 
-    currentLayout, _ = detectLayout(copiedText)
-    layoutAwareTyper = LayoutAwareTyper(currentLayout)
-    layoutAwareTyper.type(newText)
-    layoutAwareTyper.switchToOtherLayout()
-    
-    # TODO only write hebrew with hebrew layout. anything else should be written with the english layout. because bugs.
+    kb.write(newText)
 
+    changeLanguage()
     pyperclip.copy(prevClipboardContent)
 
-with GlobalHotKeys({'<19>': on_activate}) as hotKeysListener:
-    try:
-        while hotKeysListener.is_alive():
-            sleep(1)
-    except KeyboardInterrupt:
-        hotKeysListener.stop()
+
+kb.add_hotkey('pause', on_hotkey)
+kb.wait()
